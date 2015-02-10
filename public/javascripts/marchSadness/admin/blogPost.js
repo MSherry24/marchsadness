bd051bb03d555801e469b21de1c6475dc4c232e1
Mike Sherry <mikesherry24@gmail.com>
2015-02-10 15:42:10 -0600

    sorted out blog UI

--- public/javascripts/marchSadness/admin/blogPost.js
+++ public/javascripts/marchSadness/admin/blogPost.js
@@ -1,16 +1,23 @@
 /**
- * Created by MSherry on 2/10/2015.
+ * Created by mikesherry24 on 9/13/14.
  */
-$(document).ready(function () {
-    var editor = $("#texteditor").kendoEditor({
-        encoded: false,
-        tools: [
-            "bold", "italic", "underline", "strikethrough",
-            "fontSize", "foreColor", "backColor",
-            "justifyLeft", "justifyCenter", "justifyRight", "justifyFull",
-            "createLink", "unlink", "insertImage",
-            "formatting", "cleanFormatting",
-            "viewHtml"
-        ]
-    });
+
+var submitPost = function() {
+    var req = {};
+    req.postBody = $('#textarea').val();
+    req.postTitle = $('#title').val();
+    $("#SubmitButton").addClass('disabled');
+    $("#SubmitButton").html('Saving');
+
+    $.post("/marchsadness/admin/blogpost", req,
+        function () {
+            window.location.replace("/marchsadness/admin/manageBlog");
+        });
+};
+
+$(document).ready(function() {
+    // create Editor from textarea HTML element with default set of tools
+    $("#textarea").jqte();
+    $("#SubmitButton").on('click', submitPost);
 });
+
