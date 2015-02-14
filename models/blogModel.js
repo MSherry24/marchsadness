f79d210567bf144b6108799594438c5ca123f5e8
Mike Sherry <mikesherry24@gmail.com>
2015-02-14 17:53:46 -0600

    added blogmodel

---
+++ models/blogModel.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var blogModel = {};
+
+var blogPostSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    body: String,
+    timestamp: String
+});
+
+var BlogPost = mongoose.model('Blog_Post', blogPostSchema);
+blogModel.BlogPost = BlogPost;
+
+module.exports = blogModel;
